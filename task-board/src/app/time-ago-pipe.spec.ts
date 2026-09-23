20b526fafcd71ee87acc7ad59dffe7a264605fb5
Tatiana <tanya-sok-05@mail.ru>
2026-09-23 18:13:14 +0500

    upd

--- task-board/src/app/time-ago-pipe.spec.ts
+++ task-board/src/app/time-ago-pipe.spec.ts
@@ -1,8 +1,8 @@
-import { TimeAgoPipe } from './time-ago-pipe';
+import { TimeAgo } from './time-ago-pipe';
 
 describe('TimeAgoPipe', () => {
   it('create an instance', () => {
-    const pipe = new TimeAgoPipe();
+    const pipe = new TimeAgo();
     expect(pipe).toBeTruthy();
   });
 });
